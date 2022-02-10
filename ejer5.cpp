55093d3c841f40b7cec17c57d2f22d5d599f5072
mxaxel <axel.gustavo2001@outlook.com>
2022-02-10 18:10:57 +0000

    ejer5.cpp

---
+++ ejer5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "startinh program...<< flush;
+
+	cout << "This is some tenx." << endl;
+
+	cout << "Banana." << "Apple." << "orange." << endl;
+
+	cout << This is some more tex." << emdl;
+
+retunr 0;
+
+}
