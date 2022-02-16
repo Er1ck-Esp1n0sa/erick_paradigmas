5858dcea01b1ca53ff3035ff871fc03e4dfb6f52
autor <autor>
2022-02-16 03:06:02 +0000

    ajer_cap22.cpp

---
+++ ajer_cap22.cpp
@@ -0,0 +1,52 @@
+using namespase std;
+
+int main (){
+
+        cout << Array of integer>> andl;
+        cout << =============== >> endl;
+
+        int values [3];
+
+        values [0] = 88;
+        values [1] = 123;
+        values [2] = 7;
+
+        count << "Bad idea: " << values[3] << endl;
+
+	cout << values[0] << endl;
+	cout << values[1] << endl;
+	cout << values[2] << endl;
+
+	cout << endl << "Array of doubles" <<endl;
+	cout << "==================" << endl;
+
+	doble numbers[4] = {4.5, 2.3, 7.2, 8.1};
+
+	for(int i = 0; i<4; i++){
+	    cout << "elemente at index " << i << : << numbers[i] << endl;
+	}
+
+	cout << endl << "initalizing with zero values" << endl;
+	cout << "=================" << endl;
+
+	int numerArray[8] = { };
+
+	for(int i = 0; i<8; i++){
+	    cout << "elemente at index " << i << : << numberArray[i] << endl;
+	}
+
+	cout << endl << "initalizing with strings" << endl;
+	cout << "=================" << endl;
+
+	//array of string 
+	string texts[] = {"apple", "banana", "orange"}
+
+	for(int i = 0; i<3; i++){
+	    cout << "elemente at index " << i << :<< texts[i] << endl;
+	}
+
+	
+
+	return 0;
+
+}	
